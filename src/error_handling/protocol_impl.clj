69c0bb9043f13ea8acb925f59430ca3292a709eb
fr33m0nk <prashantsinha@protonmail.com>
2025-03-05 10:47:26 +0530

    Add protocol implementation

---
+++ src/error_handling/protocol_impl.clj
@@ -0,0 +1,68 @@
+(ns error-handling.protocol-impl
+  (:require [clojure.string :as str]))
+
+(defprotocol IResult
+  (then [this f])
+  (else [this f]))
+
+;; Flow does this Protocol impl on Throwable
+(extend-type Exception
+  IResult
+  (then [this _] this)
+  (else [this f]
+    (try
+      (f this)
+      (catch Exception e
+        e))))
+
+(extend-type Object
+  IResult
+  (then [this f]
+    (try
+      (f this)
+      (catch Exception ex
+        ex)))
+  (else [this _]
+    this))
+
+(defn switch
+  [x success-fn failure-fn]
+  (if (instance? Exception x)
+    (failure-fn x)
+    (success-fn x)))
+
+
+(comment
+
+  ;; Success
+  (-> 10
+      (then inc)
+      (then #(do
+               (println "Executed when previous ops does not throw exception")
+               (inc %)))
+      (else #(ex-message %)))
+
+  ;; Error
+  ;; Short circuiting when error occurs
+  (-> 10
+      (then inc)
+      (then #(/ % 0))
+      (then #(do
+               (println "Executed when previous ops does not throw exception")
+               (inc %)))
+      (else #(do
+               (println "Do whatever with exception")
+               (ex-message %))))
+
+  ;; ROP
+  ;; Error and recover and then process the rest of pipeline
+  (-> 10
+      (then inc)
+      (then #(/ % 0))
+      (then #(do
+               (println "Executed when previous ops does not throw exception")
+               (inc %)))
+      (else #(ex-message %))
+      (then str/lower-case))
+
+  )
